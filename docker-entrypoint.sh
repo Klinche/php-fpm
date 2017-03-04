0fc7cdd1b36282d22b67f989f9166c1ac8a68d2a
Daniel Brooks <dbrooks@klinche.com>
2017-03-03 22:34:38 -0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p var/cache var/logs temp/
 #TODO: Template the parameters file here...
 
 
-if [ "$ISDEV" == true ]; then
+if [ "$ISDEV" == "true" ]; then
    composer install --optimize-autoloader --no-interaction
 else
    composer install --optimize-autoloader --no-interaction --no-dev
@@ -16,18 +16,18 @@ fi
 php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction
 php bin/console --env="$ENVIRONMENT" assets:install web
 
-if [ "$ISDEV" == true ]; then
+if [ "$ISDEV" == "true" ]; then
     php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction
 else
     php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug
 fi
 
-if [ "$ISDEV" == true ]; then
+if [ "$ISDEV" == "true" ]; then
     php bin/console --env="$ENVIRONMENT" cache:warmup
 else
     php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug
 fi
 
-if [ "$ISDEV" == true ]; then
+if [ "$ISDEV" == "true" ]; then
     php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions
 fi
