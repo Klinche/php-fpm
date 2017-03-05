3706a20a869dc7f5d2ed044cd175eb355f28fc30
Daniel Brooks <dbrooks@klinche.com>
2017-03-05 15:00:56 -0800

    Some more cleanup on php image

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 set -e
 
-mkdir -p var/cache var/logs temp/
+if [ "$1" = 'php-fpm' ]; then
+    mkdir -p var/cache var/logs temp/
 
-#TODO: Template the parameters file here...
+    #TODO: Template the parameters file here...
 
 
-if [ "$ISDEV" == "true" ]; then
-   composer install --optimize-autoloader --no-interaction
-else
-   composer install --optimize-autoloader --no-interaction --no-dev
+    if [ "$ISDEV" == "true" ]; then
+       composer install --optimize-autoloader --no-interaction
+    else
+       composer install --optimize-autoloader --no-interaction --no-dev
+    fi
+
+    #TODO: WAIT FOR DATABASE
+    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction
+    php bin/console --env="$ENVIRONMENT" assets:install web
+
+    if [ "$ISDEV" == "true" ]; then
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction
+    else
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug
+    fi
+
+    if [ "$ISDEV" == "true" ]; then
+        php bin/console --env="$ENVIRONMENT" cache:warmup
+    else
+        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug
+    fi
+
+    if [ "$ISDEV" == "true" ]; then
+        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || exit 0
+    fi
 fi
-
-#TODO: WAIT FOR DATABASE
-php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction
-php bin/console --env="$ENVIRONMENT" assets:install web
-
-if [ "$ISDEV" == "true" ]; then
-    php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction
-else
-    php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug
-fi
-
-if [ "$ISDEV" == "true" ]; then
-    php bin/console --env="$ENVIRONMENT" cache:warmup
-else
-    php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug
-fi
-
-#if [ "$ISDEV" == "true" ]; then
-#    php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || exit 0
-#fi
 
 exec "$@"
