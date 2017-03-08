eb0f2d6ad2a181b6c74f4df8fe5652fa9d6d0fa8
Daniel Brooks <dbrooks@klinche.com>
2017-03-08 10:51:36 -0800

    Another try

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -euxo pipefail
+set -eo pipefail
+shopt -s nullglob
 
 if [ "$1" = 'php-fpm' ]; then
     mkdir -p var/cache var/logs temp/
@@ -8,29 +9,28 @@ if [ "$1" = 'php-fpm' ]; then
 
 
     if [ "$ISDEV" == "true" ]; then
-       composer install --optimize-autoloader --no-interaction
+       composer install --optimize-autoloader --no-interaction || echo >&2 "Composer Install Failed" && exit 1
     else
-       composer install --optimize-autoloader --no-interaction --no-dev
+       composer install --optimize-autoloader --no-interaction --no-dev || echo >&2 "Composer Install Failed" && exit 1
     fi
 
-    #TODO: WAIT FOR DATABASE
-    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction
-    php bin/console --env="$ENVIRONMENT" assets:install web
+    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || echo >&2 "Doctrine Migrations Failed" && exit 1
+    php bin/console --env="$ENVIRONMENT" assets:install web || echo >&2 "Assetic Install Failed" && exit 1
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || echo >&2 "Assetic Dump Failed" && exit 1
     else
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug
-    fi
-
-    if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" cache:warmup
-    else
-        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || echo >&2 "Assetic Dump Failed" && exit 1
     fi
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || exit 1
+        php bin/console --env="$ENVIRONMENT" cache:warmup || echo >&2 "Cache Warmup Failed" && exit 1
+    else
+        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || echo >&2 "Cache Warmup Failed" && exit 1
+    fi
+
+    if [ "$ISDEV" == "true" ]; then
+        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || echo >&2 "Doctrine Fixtures Failed" && exit 1
     fi
 fi
 
