e15c1efe2355662843ef6f30f0cedce9100170a7
Daniel Brooks <dbrooks@klinche.com>
2017-03-08 11:17:59 -0800

    this will work 😪

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,28 +9,28 @@ if [ "$1" = 'php-fpm' ]; then
 
 
     if [ "$ISDEV" == "true" ]; then
-       composer install --optimize-autoloader --no-interaction || echo >&2 "Composer Install Failed" && exit 1
+       composer install --optimize-autoloader --no-interaction || (echo >&2 "Composer Install Failed" && exit 1)
     else
-       composer install --optimize-autoloader --no-interaction --no-dev || echo >&2 "Composer Install Failed" && exit 1
+       composer install --optimize-autoloader --no-interaction --no-dev || (echo >&2 "Composer Install Failed" && exit 1)
     fi
 
-    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || echo >&2 "Doctrine Migrations Failed" && exit 1
-    php bin/console --env="$ENVIRONMENT" assets:install web || echo >&2 "Assetic Install Failed" && exit 1
+    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+    php bin/console --env="$ENVIRONMENT" assets:install web || (echo >&2 "Assetic Install Failed" && exit 1)
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || echo >&2 "Assetic Dump Failed" && exit 1
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || (echo >&2 "Assetic Dump Failed" && exit 1)
     else
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || echo >&2 "Assetic Dump Failed" && exit 1
-    fi
-
-    if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" cache:warmup || echo >&2 "Cache Warmup Failed" && exit 1
-    else
-        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || echo >&2 "Cache Warmup Failed" && exit 1
+        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || (echo >&2 "Assetic Dump Failed" && exit 1)
     fi
 
     if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || echo >&2 "Doctrine Fixtures Failed" && exit 1
+        php bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Failed" && exit 1)
+    else
+        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || (echo >&2 "Cache Warmup Failed" && exit 1)
+    fi
+
+    if [ "$ISDEV" == "true" ]; then
+        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || (echo >&2 "Doctrine Fixtures Failed" && exit 1)
     fi
 fi
 
