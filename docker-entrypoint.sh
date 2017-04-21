9afd477b670167852b3ff5a4bccdab73b9d7cd80
Daniel Brooks <dbrooks@klinche.com>
2017-04-20 23:06:28 -0700

    Removing if command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,63 +2,59 @@
 set -eo pipefail
 shopt -s nullglob
 rm -rf .php_setup
+mkdir -p var/cache var/logs temp/
 
-if [ "$1" = 'php-fpm' ]; then
+echo "parameters:" > app/config/parameters.yml
+echo "parameters:" > app/config/parameters.yml.dist
 
-    mkdir -p var/cache var/logs temp/
+while IFS='=' read -r envvar_key envvar_value
+do
+    if [[ "$envvar_key" =~ ^[symfony]+\_[a-z]+ ]]
+    then
+          replaceWith=""
+          envvar_key=${envvar_key/symfony_/${replaceWith}}
 
-    echo "parameters:" > app/config/parameters.yml
-    echo "parameters:" > app/config/parameters.yml.dist
-
-    while IFS='=' read -r envvar_key envvar_value
-    do
-        if [[ "$envvar_key" =~ ^[symfony]+\_[a-z]+ ]]
-        then
-              replaceWith=""
-              envvar_key=${envvar_key/symfony_/${replaceWith}}
-
-              if [ "$envvar_value" == "true" ] || [ "$envvar_value" == "false" ]
-              then
-                  echo "    $envvar_key: $envvar_value" >> app/config/parameters.yml
-                  echo "    $envvar_key: $envvar_value" >> app/config/parameters.yml.dist
-              else
-                  echo "    $envvar_key: '$envvar_value'" >> app/config/parameters.yml
-                  echo "    $envvar_key: '$envvar_value'" >> app/config/parameters.yml.dist
-              fi
+          if [ "$envvar_value" == "true" ] || [ "$envvar_value" == "false" ]
+          then
+              echo "    $envvar_key: $envvar_value" >> app/config/parameters.yml
+              echo "    $envvar_key: $envvar_value" >> app/config/parameters.yml.dist
+          else
+              echo "    $envvar_key: '$envvar_value'" >> app/config/parameters.yml
+              echo "    $envvar_key: '$envvar_value'" >> app/config/parameters.yml.dist
+          fi
 
 
-        fi
-    done < <(env)
-
-
-    if [ "$ISDEV" == "true" ]; then
-       composer install --optimize-autoloader --no-interaction || (echo >&2 "Composer Install Dev Failed" && exit 1)
-    else
-       composer install --optimize-autoloader --no-interaction --no-dev || (echo >&2 "Composer Install Prod Failed" && exit 1)
     fi
+done < <(env)
 
-    php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
-    php bin/console --env="$ENVIRONMENT" assets:install web || (echo >&2 "Assetic Install Failed" && exit 1)
 
-    if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || (echo >&2 "Assetic Dump Dev Failed" && exit 1)
-    else
-        php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || (echo >&2 "Assetic Dump Prod Failed" && exit 1)
-    fi
+if [ "$ISDEV" == "true" ]; then
+   composer install --optimize-autoloader --no-interaction || (echo >&2 "Composer Install Dev Failed" && exit 1)
+else
+   composer install --optimize-autoloader --no-interaction --no-dev || (echo >&2 "Composer Install Prod Failed" && exit 1)
+fi
 
-    if [ "$ISDEV" == "true" ]; then
-        php bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
-    else
-        php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || (echo >&2 "Cache Warmup Prod Failed" && exit 1)
-    fi
+php bin/console --env="$ENVIRONMENT" doctrine:migrations:migrate --no-interaction || (echo >&2 "Doctrine Migrations Failed" && exit 1)
+php bin/console --env="$ENVIRONMENT" assets:install web || (echo >&2 "Assetic Install Failed" && exit 1)
 
-    if [ "$ISDEV" == "true" ]; then
-            #Save off the db dir number.
-            numdirs=$(ls -l "$DB_DIR" | grep -v ^d | wc -l | xargs)
-            echo "Number of db directories is $numdirs"
-            if  [ $numdirs -le 2 ]; then
-                php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || (echo >&2 "Doctrine Fixtures Failed" && exit 1)
-            fi
+if [ "$ISDEV" == "true" ]; then
+    php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction || (echo >&2 "Assetic Dump Dev Failed" && exit 1)
+else
+    php bin/console --env="$ENVIRONMENT" assetic:dump --no-interaction --no-debug || (echo >&2 "Assetic Dump Prod Failed" && exit 1)
+fi
+
+if [ "$ISDEV" == "true" ]; then
+    php bin/console --env="$ENVIRONMENT" cache:warmup || (echo >&2 "Cache Warmup Dev Failed" && exit 1)
+else
+    php bin/console --env="$ENVIRONMENT" cache:warmup --no-debug || (echo >&2 "Cache Warmup Prod Failed" && exit 1)
+fi
+
+if [ "$ISDEV" == "true" ]; then
+    #Save off the db dir number.
+    numdirs=$(ls -l "$DB_DIR" | grep -v ^d | wc -l | xargs)
+    echo "Number of db directories is $numdirs"
+    if  [ $numdirs -le 2 ]; then
+        php bin/console --env="$ENVIRONMENT" doctrine:fixtures:load --no-interaction --multiple-transactions || (echo >&2 "Doctrine Fixtures Failed" && exit 1)
     fi
 fi
 
